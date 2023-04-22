3a141dbc88c204015fc9adbc3c1331708da33aae
willams192 <wiilhams82@gmail.com>
2023-04-22 15:34:45 -0300

    FixScrip:: corrigindo e adcionando o style do texto

--- lib/questao.dart
+++ lib/questao.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
   final String texto;
+  final TextStyle estilo;
 
-  Questao(this.texto);
+  const Questao(this.texto, {required this.estilo});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: EdgeInsets.all(20),
+      margin: const EdgeInsets.all(20),
       child: Text(
         texto,
-        style: TextStyle(fontSize: 28),
+        style: estilo ?? TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
     );
